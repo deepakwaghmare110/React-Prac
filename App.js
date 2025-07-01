4be96503d4316974f56de78076c07ddcaa1f684f
Deepak Waghmare <deepakwaghmare110@gmail.com>
2025-07-01 17:53:43 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Heading = <h1>Hello World</h1>;
-
 // Functional Component
 
-const HeadingComponent = () => {
-  return <h1>Hello world from ReactJS</h1>;
+const Heading = () => {
+  return (
+    <div className="div">
+      <h1>React series part 1</h1>
+    </div>
+  );
+};
+
+const Heading2 = () => {
+  return (
+    <div className="div2">
+      <h1>React series part 2</h1>
+      <Heading />
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<Heading2 />);

f580db44bf16d9e3b6dcbdbbbd132ad2867111c3
Deepak Waghmare <deepakwaghmare110@gmail.com>
2025-07-04 22:08:23 +0530

    filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,27 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import RestroCard from "./RestroCard";
 import resObj from "../utils/mockData";
 
 const Body = () => {
+  const [restro, setRestro] = useState(resObj);
+
+  const handleFilter = () => {
+    const filtered = resObj.filter((res) => {
+      return res.rating > 4;
+    });
+    setRestro(filtered);
+  };
+
   return (
     <div className="body">
       <div className="filter">
-        <button
-          className="filter-btn"
-          onClick={() => {
-            filterRes = resObj.filter((res) => {
-              return res.rating > 4;
-            });
-            console.log(filterRes);
-          }}
-        >
+        <button className="filter-btn" onClick={handleFilter}>
           Top Rated Resturants Near me
         </button>
       </div>
       <div className="res-container">
-        {resObj.map((resturant) => (
-          <RestroCard resData={resturant} key={resturant.Id} />
-        ))}
+        {restro.map(
+          (
+            resturant // on initial level restro is equal resObj but later it gets chnaged when filter method is applied.
+          ) => (
+            <RestroCard resData={resturant} key={resturant.Id} />
+          )
+        )}
       </div>
     </div>
   );

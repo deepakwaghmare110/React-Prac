3f328f6c384dd119c11f8132a4bd4f66f662894d
Deepak Waghmare <deepakwaghmare110@gmail.com>
2025-07-04 15:31:13 +0530

    map

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,45 @@
 import React from "react";
 import RestroCard from "./RestroCard";
-import resObj from "./resData"; // move data to a separate file
 
 const Body = () => {
+  const resObj = [
+    {
+      Id: "0",
+      name: "Plum By Bent Chair",
+      city: "New Delhi",
+      rating: "4.6",
+      cuisines: "Asian",
+    },
+    {
+      Id: "1",
+      name: "AIR- An Ivory Region",
+      city: "New Delhi",
+      rating: "4.0",
+      cuisines: "North Indian",
+    },
+    {
+      Id: "2",
+      name: "Jay Malhar Dhaba",
+      city: "Mumbai",
+      rating: "9",
+      cuisines: "Maharashtrian, Malvani",
+    },
+    {
+      Id: "3",
+      name: "Malvan Tandka",
+      city: "Kalyan",
+      rating: "9.2",
+      cuisines: "Malvani",
+    },
+  ];
+
   return (
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <RestroCard />
-        <RestroCard />
-        <RestroCard />
-        <RestroCard />
-        <RestroCard />
-        <RestroCard />
+        {resObj.map((resturant) => (
+          <RestroCard resData={resturant} key={resturant.Id} />
+        ))}
       </div>
     </div>
   );
--- src/components/RestroCard.js
+++ src/components/RestroCard.js
@@ -1,17 +1,16 @@
 import React from "react";
 
-const RestroCard = () => {
+const RestroCard = ({ resData }) => {
   return (
     <div className="res">
-      <h3>Jay Malhar Dhaba</h3>
+      <h3>{resData.name}</h3>
       <img
         className="res-img"
         src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/86/c2/c2/photo0jpg.jpg?w=600&h=400&s=1"
-        alt="restaurant"
+        alt={resData.name}
       />
-      <h4>9.0 Rating</h4>
-      <h4>Zomato</h4>
-      <h4>Swiggy</h4>
+      <h4>City:{resData.city}</h4>
+      <h4>Rating:{resData.rating}</h4>
     </div>
   );
 };

ab6b51f72c8f3a37de8d7e978654286034ade9f2
Deepak Waghmare <deepakwaghmare110@gmail.com>
2025-07-20 15:03:57 +0530

    path added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Body from "./components/Body";
 import Header from "./components/Header";
+import About from "./components/About";
 
 const AppLayout = () => {
   return (
@@ -13,5 +14,16 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>This is our about us page.</p>
+    </div>
+  );
+};
+
+export default About;

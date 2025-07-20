622367416d1e5defc8816d95a8545794fb06a088
Deepak Waghmare <deepakwaghmare110@gmail.com>
2025-07-20 17:17:27 +0530

    initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
-
+import { Link } from "react-router-dom";
 const Header = () => {
   const [btn, setBtn] = useState("Login");
   return (
@@ -11,8 +11,12 @@ const Header = () => {
       <div className="nav-items">
         <ul>
           <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
           <li>Cart</li>
           <button
             onClick={() =>
